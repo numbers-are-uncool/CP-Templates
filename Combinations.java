30dd6816209ef4bf629298ed10ce4a326a3fcb12
purple-affogato <110414458+purple-affogato@users.noreply.github.com>
2022-11-28 17:49:00 -0500

    rename and update file

--- Combinations.java
+++ Combinations.java
@@ -1,11 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Combination_Practice {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
 	static int ctr = 0;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		int a[] = { 52, 56, 32, 67 };
 		List<Integer> ans = new ArrayList<Integer>();
 		combine(a, 0, ans);
@@ -22,31 +20,5 @@ public class Combination_Practice {
 		l.add(s[c]);
 		combine(s, c + 1, l);
 		l.remove(l.size() - 1);
-	}
-
-	static String next() throws IOException {
-		while (st == null || !st.hasMoreTokens())
-			st = new StringTokenizer(br.readLine().trim());
-		return st.nextToken();
-	}
-
-	static long readLong() throws IOException {
-		return Long.parseLong(next());
-	}
-
-	static int readInt() throws IOException {
-		return Integer.parseInt(next());
-	}
-
-	static double readDouble() throws IOException {
-		return Double.parseDouble(next());
-	}
-
-	static char readCharacter() throws IOException {
-		return next().charAt(0);
-	}
-
-	static String readLine() throws IOException {
-		return br.readLine().trim();
 	}
 }
