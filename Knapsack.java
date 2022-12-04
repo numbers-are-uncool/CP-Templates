33dc6b0613ff1dbfe1b0ed39a63d30b2a0ed078c
purple-affogato <110414458+purple-affogato@users.noreply.github.com>
2022-12-04 12:01:53 -0500

    Update and rename Knapsack_1.java to Knapsack.java

--- Knapsack.java
+++ Knapsack.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class Knapsack_1 {
+public class Knapsack {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
@@ -14,28 +14,16 @@ public class Knapsack_1 {
 			for(int j=w;j>=a[i].weight;j--) {
 				ans[j] = Math.max(ans[j], ans[j - a[i].weight] + a[i].value);
 			}
-			//System.out.println(Arrays.toString(ans));
 		}
 		System.out.println(ans[w]);
-		System.out.println(Arrays.toString(ans));
 	}
-	private static class Item implements Comparable<Item> {
+	private static class Item {
 		int weight;
 		long value;
 		public Item (int w, long v) {
 			weight = w;
 			value = v;
 		}
-		@Override
-		public int compareTo(Item o) {
-			if(value > o.value)
-				return 1;
-			else if(value < o.value)
-				return -1;
-			else
-				return 0;
-		}
-		
 	}
 	static String next() throws IOException {
 		while (st == null || !st.hasMoreTokens()) 
