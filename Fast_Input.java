677f265fd09b732f90c5fb09b976e807392b3cee
purple-affogato <110414458+purple-affogato@users.noreply.github.com>
2022-12-04 12:03:46 -0500

    Create Fast_Input.java

---
+++ Fast_Input.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class Knapsack {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+	  //put input here
+    //uses BufferedReader & StringTokenizer
+    //written by my teacher, Bruce :D
+  }
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
